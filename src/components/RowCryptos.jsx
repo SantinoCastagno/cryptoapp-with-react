ddf0f47b6d8bc394bfa98338d20f31b0b54cbc85
Santino Castagno <santinocastagno.dev@gmail.com>
2021-08-26 14:49:12 -0300

    Improve rowsCryptos

--- src/components/RowCryptos.jsx
+++ src/components/RowCryptos.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-function RowCryptos({crypto, index}) {
+function RowCryptos({ crypto, index }) {
   return (
     <tr>
       <td>{index}</td>
       <td>{crypto.name}</td>
+      <td>{crypto.symbol}</td>
+      <td>{crypto.current_price}</td>
+      <td>{crypto.price_change_percentage_24h}%</td>
+      <td>{crypto.total_volume}</td>
     </tr>
   );
 }
--- src/components/TableCryptos.jsx
+++ src/components/TableCryptos.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import RowCryptos from "./RowCryptos";
 
-const titles = ["#", "Crypto name", "Price", "Price Change", "24h volume"];
+const titles = ["#", "Crypto name", "Symbol",  "Price", "Price Change", "24h volume"];
 
 const TableCryptos = ({ cryptos }) => {
   return (
     <table className="table table-dark mt-4 table-hover">
       <thead>
         <tr>
-          {titles.map((title) => (
-            <th>{title}</th>
+          {titles.map((title, index) => (
+            <th key={index}>{title}</th>
           ))}
         </tr>
       </thead>
       <tbody>
         {cryptos.map((crypto, index) => (
-          <RowCryptos key={index + 1} crypto={crypto} index={index + 1} />
+          <RowCryptos key={crypto.symbol} crypto={crypto} index={index + 1} />
         ))}
       </tbody>
     </table>
